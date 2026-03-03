cf6dc9d8489a55ba33fc34f440bf7f4b6e43e5dc
cgalean0 <cngaleano56@gmail.com>
2026-03-02 22:11:48 -0300

    Feat: Initial structure of the mini shop

---
+++ orders/src/main/java/com/argos/orders/ArgosOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.argos.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgosOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgosOrdersApplication.class, args);
+	}
+
+}
---
+++ orders/src/test/java/com/argos/orders/ArgosOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argos.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgosOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
