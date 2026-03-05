3c52b234466a97f68b136ec3e4655eee41d0a2d9
cgalean0 <cngaleano56@gmail.com>
2026-03-04 21:06:03 -0300

    FIX: add the config of the rabbitMQ creation of queues

--- sentinel/src/main/java/com/argos/sentinel/RabbitMQConfig.java
+++ sentinel/src/main/java/com/argos/sentinel/RabbitMQConfig.java
@@ -1,5 +1,9 @@
 package com.argos.sentinel;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +15,50 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 
 @Configuration
 public class RabbitMQConfig {
+    public static final String QUEUE_ALERT = "argos.alert.queue";
+    public static final String ALERT_EXCHANGE = "alert.exchange";
+    public static final String RK_ALERT = "argos.alert.#";
+
+    public static final String QUEUE_SALE = "argos.sales.queue";    
+    public static final String EXCHANGE_SOLD = "shop.exchange";
+    public static final String RK_SALES = "shop.event.sold";
+
+    /*
+    * Notifications queue
+    */
+    @Bean
+    public Queue alertQueue() {
+        return new Queue(QUEUE_ALERT, true);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(ALERT_EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue alertQueue, TopicExchange exchange) {
+        return BindingBuilder.bind(alertQueue).to(exchange).with(RK_ALERT);
+    }
+
+    /**
+     * Sales queue
+     */
+    @Bean
+    public Queue salesQueue() {
+        return new Queue(QUEUE_SALE, true);
+    }
+
+    @Bean
+    public TopicExchange exchangeSold() {
+        return new TopicExchange(EXCHANGE_SOLD);
+    }
+
+    @Bean
+    public Binding bindingSales(Queue salesQueue, TopicExchange exchange) {
+        return BindingBuilder.bind(salesQueue).to(exchange).with(RK_SALES);
+    }
+
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
@@ -18,6 +66,7 @@ public class RabbitMQConfig {
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return mapper;
     }
+
 
     @Bean
     public MessageConverter jsonMessageConverter(ObjectMapper objectMapper) {
